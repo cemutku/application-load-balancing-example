5b365be18a5cc2f4abf88b155c71df85702924e8
cemutku <33598193+cemutku@users.noreply.github.com>
2025-05-13 00:44:38 +0200

    added per path rate limits
    added docker-compose deploy-replicas
    
    added circuit breaker

--- ServiceA/Program.cs
+++ ServiceA/Program.cs
@@ -1,7 +1,23 @@
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Extensions.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
+builder.Services.AddHttpClient("ServiceB", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5001");
+})
+.AddPolicyHandler(GetRetryPolicy())
+.AddPolicyHandler(GetCircuitBreakerPolicy());
+
+builder.Logging.Configure(options =>
+{
+    options.ActivityTrackingOptions = ActivityTrackingOptions.SpanId | ActivityTrackingOptions.TraceId;
+});
+
 
 var app = builder.Build();
 
@@ -21,10 +37,78 @@ app.Use(async (context, next) =>
 
 app.MapGet("/", (HttpContext context) =>
 {
+    string containerName = Environment.MachineName;
     var port = context.Connection.LocalPort;
-    return $"Hello from Service A on port {port}";
+    return $"Hello from Service A on port {port} - {containerName}";
 });
+
+app.MapGet("/serviceb-hello", async (
+    IHttpClientFactory httpClientFactory,
+    ILogger<Program> logger) =>
+{
+    try
+    {    
+        var httpClient = httpClientFactory.CreateClient("ServiceB");            
+        var response = await httpClient.GetAsync("/");
+        
+        logger.LogWarning("called B ");
+        
+        response.EnsureSuccessStatusCode();
+        return Results.Text(await response.Content.ReadAsStringAsync());
+    }
+    catch (BrokenCircuitException)
+    {
+        logger.LogWarning("Circuit breaker is open — skipping call to ServiceB");
+        var error = new
+        {
+            status = 503,
+            message = "Service is temporarily unavailable. Please try again later."
+        };
+
+        return Results.Json(error, statusCode: 503);
+    }
+    catch (System.Exception ex)
+    {
+        logger.LogError(ex, "Unhandled error when calling ServiceB");
+        throw;
+    }
+});
+
 
 app.MapGet("/health", () => Results.Ok("A is Healthy"));
 
 app.Run("http://+:80");
+
+static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .WaitAndRetryAsync(
+            retryCount: 2,
+            sleepDurationProvider: attempt => TimeSpan.FromMilliseconds(300 * attempt), // exponential backoff
+            onRetry: (outcome, timespan, retryAttempt, context) =>
+            {
+                Console.WriteLine($"Retry {retryAttempt} after {timespan.TotalMilliseconds}ms due to {outcome.Exception?.Message}");
+            });
+}
+
+static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .CircuitBreakerAsync(
+            handledEventsAllowedBeforeBreaking: 3,         // break after 3 consecutive failures
+            durationOfBreak: TimeSpan.FromSeconds(15),     // stay open for 15 seconds
+            onBreak: (outcome, timespan) =>
+            {
+                Console.WriteLine($"Circuit broken for {timespan.TotalSeconds}s due to: {outcome.Exception?.Message}");
+            },
+            onReset: () =>
+            {
+                Console.WriteLine("Circuit reset.");
+            },
+            onHalfOpen: () =>
+            {
+                Console.WriteLine("Circuit is half-open: testing...");
+            });
+}
--- ServiceB/Program.cs
+++ ServiceB/Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
+builder.Logging.Configure(options =>
+{
+    options.ActivityTrackingOptions = ActivityTrackingOptions.SpanId | ActivityTrackingOptions.TraceId;
+});
+
 var app = builder.Build();
 
 app.UseRouting();
@@ -19,12 +24,24 @@ app.Use(async (context, next) =>
         sw.ElapsedMilliseconds);
 });
 
-app.MapGet("/", (HttpContext context) =>
+app.MapGet("/", (HttpContext context, ILogger<Program> logger) =>
 {
-    var port = context.Connection.LocalPort;
-    return $"Hello from Service B on port {port}";
+    var error = new
+    {
+        status = 503,
+        message = "Service B is temporarily unavailable. Please try again later."
+    };
+    logger.LogWarning("Service B is temporarily unavailable. Please try again later.");
+
+
+    return Results.Json(error, statusCode: 503);
+    
+    // string containerName = Environment.MachineName;
+    // var port = context.Connection.LocalPort;
+    // return $"Hello from Service B on port {port} - {containerName}";
 });
 
 app.MapGet("/health", () => Results.Ok("B is Healthy"));
 
-app.Run("http://+:80");
+// app.Run("http://+:80");
+app.Run();
